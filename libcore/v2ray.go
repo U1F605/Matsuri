4cf18ee698d6d28ef13c659a40c5099cefef1a09
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-26 18:59:39 +0800

    Update v2ray.go

--- libcore/v2ray.go
+++ libcore/v2ray.go
@@ -205,14 +205,6 @@ func (v2ray *V2RayInstance) setupDialer() {
 
 				if nekoutils.In(tryDomains, domain) {
 					// first try A
-					_ips, err := doh.LookupManyDoH(domain, 1)
-					if err != nil {
-						// then try AAAA
-						_ips, err = doh.LookupManyDoH(domain, 28)
-						if err != nil {
-							return nil, err
-						}
-					}
 					ips := _ips.([]net.IP)
 					staticHosts[domain] = ips
 					return ips, nil
