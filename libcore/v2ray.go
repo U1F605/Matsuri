801961a2a63905a5ac0d5cceaba1e91970025c36
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-26 20:53:01 +0800

    Update v2ray.go

--- libcore/v2ray.go
+++ libcore/v2ray.go
@@ -17,7 +17,6 @@ import (
 
 	core "github.com/v2fly/v2ray-core/v5"
 	"github.com/v2fly/v2ray-core/v5/app/dispatcher"
-	"github.com/v2fly/v2ray-core/v5/app/observatory"
 	"github.com/v2fly/v2ray-core/v5/common/buf"
 	"github.com/v2fly/v2ray-core/v5/common/net"
 	"github.com/v2fly/v2ray-core/v5/features/dns"
