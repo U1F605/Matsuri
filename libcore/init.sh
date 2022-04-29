240d8496f3a4d36b6aea89fb3c71ae7067417dd5
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-04-29 19:23:11 +0800

    Update init.sh

--- libcore/init.sh
+++ libcore/init.sh
@@ -9,7 +9,7 @@ go get -v -d
 
 # Install gomobile
 if [ ! -f "$GOPATH/bin/gomobile" ]; then
-    go install -v github.com/sagernet/gomobile/cmd/gomobile@v0.0.0-20210905032500-701a995ff844
+    go install -v golang.org/x/mobile/cmd/gomobile@latest
     go install -v github.com/sagernet/gomobile/cmd/gobind@v0.0.0-20210905032500-701a995ff844
 fi
 
