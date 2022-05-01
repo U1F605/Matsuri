123219ad3830a5d353e28ff2e7214ac6706771f5
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-01 11:37:32 +0800

    Update build.sh

--- libcore/build.sh
+++ libcore/build.sh
@@ -11,7 +11,7 @@ rm -rf $BUILD/android \
   $BUILD/javac-output \
   $BUILD/src
 
-gomobile bind -v -cache $(realpath build) -trimpath -ldflags='-s -w' . || exit 1
+gomobile bind -v -trimpath -ldflags='-s -w' . || exit 1
 rm -r libcore-sources.jar
 
 proj=../app/libs
