c859a56c881f60fb0d429c6c635adab1d4f856a9
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-02 13:50:35 +0800

    Update build.sh

--- libcore/build.sh
+++ libcore/build.sh
@@ -3,10 +3,3 @@
 source .github/env.sh
 
 [ $rel ] || sed -i "s/buildDate .*/buildDate := \"`date +'%Y%m%d'`\"/g" date.go
-
-BUILD="build"
-
-rm -rf $BUILD/android \
-  $BUILD/java \
-  $BUILD/javac-output \
-  $BUILD/src
