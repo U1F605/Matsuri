c53c20f366c6c80340b292bfdba8b7f8c611b790
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-06 10:30:28 +0800

    Update TypeMap.kt

--- app/src/main/java/io/nekohasekai/sagernet/fmt/TypeMap.kt
+++ app/src/main/java/io/nekohasekai/sagernet/fmt/TypeMap.kt
@@ -32,7 +32,6 @@ object TypeMap : HashMap<String, Int>() {
         this["trojan-go"] = ProxyEntity.TYPE_TROJAN_GO
         this["naive"] = ProxyEntity.TYPE_NAIVE
         this["hysteria"] = ProxyEntity.TYPE_HYSTERIA
-        this["ssh"] = ProxyEntity.TYPE_SSH
         this["wg"] = ProxyEntity.TYPE_WG
         this["neko"] = ProxyEntity.TYPE_NEKO
     }
