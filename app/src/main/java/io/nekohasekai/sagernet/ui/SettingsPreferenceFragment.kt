1665f469313e236ee7ddfb53eaffc6c60d76700f
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-07 22:41:24 +0800

    Update SettingsPreferenceFragment.kt

--- app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
+++ app/src/main/java/io/nekohasekai/sagernet/ui/SettingsPreferenceFragment.kt
@@ -208,7 +208,6 @@ class SettingsPreferenceFragment : PreferenceFragmentCompat() {
         }
 
         val providerTrojan = findPreference<SimpleMenuPreference>(Key.PROVIDER_TROJAN)!!
-        val providerWireguard = findPreference<SimpleMenuPreference>(Key.PROVIDER_WIREGUARD)!!
         val muxProtocols = findPreference<MultiSelectListPreference>(Key.MUX_PROTOCOLS)!!
 
         muxProtocols.apply {
