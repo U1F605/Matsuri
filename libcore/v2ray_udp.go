6b8c18c12e9dd37477216085e6d68f7b50bb8e73
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-10 23:47:38 +0800

    tun

--- libcore/v2ray_udp.go
+++ libcore/v2ray_udp.go
@@ -3,7 +3,6 @@ package libcore
 import (
 	"context"
 	"libcore/comm"
-	"libcore/tun"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -20,7 +19,7 @@ import (
 // this file is for v2ray's udp
 
 func (instance *V2RayInstance) newDispatcherConn(ctx context.Context, destinationConn net.Destination, destinationV2ray net.Destination,
-	writeBack tun.WriteBack, timeout time.Duration, workerN int,
+	timeout time.Duration, workerN int,
 ) (*dispatcherConn, error) {
 	ctx, cancel := context.WithCancel(core.WithContext(ctx, instance.core))
 	link, err := instance.dispatcher.Dispatch(ctx, destinationV2ray)
@@ -33,7 +32,6 @@ func (instance *V2RayInstance) newDispatcherConn(ctx context.Context, destinatio
 		link:      link,
 		ctx:       ctx,
 		cancel:    cancel,
-		writeBack: writeBack,
 	}
 	c.timer = signal.CancelAfterInactivity(ctx, func() {
 		comm.CloseIgnore(c)
@@ -55,7 +53,6 @@ type dispatcherConn struct {
 	ctx    context.Context
 	cancel context.CancelFunc
 
-	writeBack tun.WriteBack //downlink
 
 	stats *myStats // traffic stats
 }
@@ -96,18 +93,9 @@ func (c *dispatcherConn) handleDownlink() {
 				src.Address = net.AnyIP
 			}
 
-			n, err := c.writeBack(buffer.Bytes(), &net.UDPAddr{
-				IP:   src.Address.IP(),
-				Port: int(src.Port),
-			})
-
 			buffer.Release()
 
-			if err == nil {
-				if c.stats != nil {
-					atomic.AddUint64(c.stats.downlink, uint64(n))
-				}
-			} else {
+			if err != nil {
 				return
 			}
 		}
