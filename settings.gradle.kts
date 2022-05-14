d11d0bf4184f5e2d329a7c4dcaf109a00bf79e32
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-14 18:14:52 +0800

    plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ include(":library:include")
 include(":library:proto")
 include(":library:proto-stub")
 
-include(":plugin:api")
-
 include(":external:preferencex:preferencex")
 include(":external:preferencex:preferencex-simplemenu")
 include(":external:preferencex:flexbox")
