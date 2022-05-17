54241d84d64fef38d17530a45b617a9bc66b3c1c
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-17 18:18:49 +0800

    Update VpnService.kt

--- app/src/main/java/io/nekohasekai/sagernet/bg/VpnService.kt
+++ app/src/main/java/io/nekohasekai/sagernet/bg/VpnService.kt
@@ -277,70 +277,6 @@ class VpnService : BaseVpnService(),
         conn = builder.establish() ?: throw NullConnectionException()
     }
 
-    // this is sekaiResolver
-    override fun lookupIP(network: String, domain: String): String {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            return runBlocking {
-                suspendCoroutine { continuation ->
-                    val signal = CancellationSignal()
-                    val callback = object : DnsResolver.Callback<Collection<InetAddress>> {
-                        @Suppress("ThrowableNotThrown")
-                        override fun onAnswer(answer: Collection<InetAddress>, rcode: Int) {
-                            // libcore/v2ray.go
-                            when {
-                                answer.isNotEmpty() -> {
-                                    continuation.tryResume((answer as Collection<InetAddress?>).mapNotNull { it?.hostAddress }
-                                        .joinToString(","))
-                                }
-                                rcode == 0 -> {
-                                    // fuck AAAA no record
-                                    // features/dns/client.go
-                                    continuation.tryResume("")
-                                }
-                                else -> {
-                                    // Need return rcode
-                                    // proxy/dns/dns.go
-                                    continuation.tryResumeWithException(Exception("$rcode"))
-                                }
-                            }
-                        }
-
-                        override fun onError(error: DnsResolver.DnsException) {
-                            continuation.tryResumeWithException(error)
-                        }
-                    }
-                    val type = when {
-                        network.endsWith("4") -> DnsResolver.TYPE_A
-                        network.endsWith("6") -> DnsResolver.TYPE_AAAA
-                        else -> null
-                    }
-                    if (type != null) {
-                        DnsResolver.getInstance().query(
-                            underlyingNetwork,
-                            domain,
-                            type,
-                            DnsResolver.FLAG_EMPTY,
-                            Dispatchers.IO.asExecutor(),
-                            signal,
-                            callback
-                        )
-                    } else {
-                        DnsResolver.getInstance().query(
-                            underlyingNetwork,
-                            domain,
-                            DnsResolver.FLAG_EMPTY,
-                            Dispatchers.IO.asExecutor(),
-                            signal,
-                            callback
-                        )
-                    }
-                }
-            }
-        } else {
-            throw Exception("114514")
-        }
-    }
-
     val appStats = mutableListOf<AppStats>()
 
     override fun updateStats(stats: AppStats) {
@@ -351,7 +287,6 @@ class VpnService : BaseVpnService(),
         if (!DataStore.appTrafficStatistics) return
         val tun = getTun() ?: return
         appStats.clear()
-        tun.readAppTraffics(this)
         val toUpdate = mutableListOf<StatsEntity>()
         val all = SagerDatabase.statsDao.all().associateBy { it.packageName }
         for (stats in appStats) {
