1cb626b478fdd53be5cb72404c63c53c512e01cd
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-19 18:27:36 +0800

    Update clone.sh

--- buildScript/lib/core/clone.sh
+++ buildScript/lib/core/clone.sh
@@ -1,5 +1,8 @@
 cd ..
-[ -d v2ray-core ] && exit 0
+[ -d v2ray ] && exit 0
 rm -rf v2ray-core
+pwd
+ls -l
 git clone https://github.com/matsuridayo/v2ray-core.git
-cd v2ray
+mv vmessocketForAndroid/v2ray/ vmessocketForAndroid/
+ls -l
