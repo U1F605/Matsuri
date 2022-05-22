ca26d4d771c0a06066b2752f216288b24e6c024a
U1F605 <30045531+U1F605@users.noreply.github.com>
2022-05-22 12:33:11 +0800

    Update strategy_leastping.go

--- v2ray/app/router/strategy_leastping.go
+++ v2ray/app/router/strategy_leastping.go
@@ -7,7 +7,6 @@ import (
 	"context"
 
 	core "github.com/v2fly/v2ray-core/v5"
-	"github.com/v2fly/v2ray-core/v5/app/observatory"
 	"github.com/v2fly/v2ray-core/v5/common"
 	"github.com/v2fly/v2ray-core/v5/features"
 	"github.com/v2fly/v2ray-core/v5/features/extension"
